e4d4248270362afab4620ce2fdab0779f405c36c
LittleToxicBoy <ezequielabsoft@micorreo.upp.edu.mx>
2021-07-05 01:52:36 -0500

    creacion vista registro niños

--- app/src/main/java/com/example/club/registro_n.java
+++ app/src/main/java/com/example/club/registro_n.java
@@ -1,17 +1,13 @@
 package com.example.club;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class     vista2 extends AppCompatActivity {
-
+public class registro_n extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vista_dash);
+        setContentView(R.layout.vista_regninos);
     }
-
 }
